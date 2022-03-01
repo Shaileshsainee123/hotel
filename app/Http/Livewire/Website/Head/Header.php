c50c6991b9d6885f59cd0c72753bac7d76dd8400
Shaileshsainee123 <sk19810848@gmail.com>
2022-03-01 15:39:21 +0530

    day1

---
+++ app/Http/Livewire/Website/Head/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Website\Head;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.website.head.header');
+    }
+}
